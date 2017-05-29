1ca68efd58168fa27eddb84689c4b5e41ee095c0
Elena Mateos Lopez <elena.mateos1@gmail.com>
2017-05-29 12:17:59 +0200

    added animation at menu, first version

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ function menuButton(){
 var menu = document.querySelector('#menu');
 if (menu.style.display == 'none') {
   menu.style.display = 'block';
-  console.log('llega');
+
 }
 else {
   menu.style.display = 'none';
 }
 }
-
-
 
 button.addEventListener('click', menuButton);
